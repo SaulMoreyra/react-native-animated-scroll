fce6f4a54649298f8377d1ac5650cebd27bfbffa
SaulMoreyra <armhacam21@gmail.com>
2023-02-20 17:04:46 -0600

    feat: add scroll animated with reanimated

--- App.tsx
+++ App.tsx
@@ -1,18 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import Page from "./components/Page";
+
+const WORDS = ["What's up", "mobile", "devs"];
 
 export default function App() {
+  const translateX = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    translateX.value = event.contentOffset.x;
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Animated.ScrollView
+      onScroll={scrollHandler}
+      scrollEventThrottle={16}
+      horizontal
+      style={styles.container}
+    >
+      {WORDS.map((word, index) => (
+        <Page
+          key={index.toString()}
+          index={index}
+          title={word}
+          translateX={translateX}
+        />
+      ))}
+    </Animated.ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
